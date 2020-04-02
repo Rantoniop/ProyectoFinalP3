aef5bfcfa03ced52c52008186d7301c7d6d87fb1
Ruddy <ruddyantonio@gmail.com>
2020-04-02 13:11:42 -0400

    Se han creado el CRUD de Medicos, establecido las propiedades de la clase Medicos, integrado al proyecto el EntityFramework, crado la clase MCSystemContext y agragado el DbSet para la creacion de la tabla.

---
+++ ProyectoFinalP3/Models/MCSystemContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProyectoFinalP3.Models
+{
+    public class MCSystemContext: DbContext
+    {
+        public MCSystemContext()
+            : base("MCSystemDB")
+        {
+            
+        }
+
+        public DbSet<Medicos> Medicos { get; set; }
+ 
+    }
+}
---
+++ ProyectoFinalP3/Models/Medicos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalP3.Models
+{
+    public class Medicos
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Exequatur { get; set; }
+        public string Especialidad { get; set; }
+
+    }
+}
